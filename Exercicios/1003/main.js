e0cf289b31d0b06a5758d30f99af5c09376ed4d0
DiegoVeiga <diegoroberto27@hotmail.com>
2020-11-02 12:48:26 -0300

    Exercicio 3

---
+++ Exercicios/1003/main.js
@@ -0,0 +1,25 @@
+elementButton = document.querySelector('.btnMedia');
+
+elementButton.onclick = ()=>{
+    var elementInput1 = document.querySelector('.nota1');
+    var elementInput2 = document.querySelector('.nota2');
+    
+    v1 = Number(elementInput1.value);
+    v2 = Number(elementInput2.value);
+    
+    let final = Calculamedia(v1, v2)
+   alert(final);
+
+
+}
+
+function Calculamedia(v1,v2){
+    let nota1 = v1 * 0.35;
+    let nota2  = v2 * 0.75;
+    
+    let notaFinal = (nota1 + nota2);
+     let nf =notaFinal.toFixed(1);
+
+     return nf ;
+
+}
